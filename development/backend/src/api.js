78e3e09d85ca091fe29ba977813e507de378ec8c
hayashi-ay <go12limchangyong@gmail.com>
2022-04-17 02:06:04 +0900

    fix: bulkInsert

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -93,14 +93,17 @@ const postRecords = async (req, res) => {
     ],
   );
 
+  let values = [];
+
   for (const e of body.fileIdList) {
-    await pool.query(
-      `insert into record_item_file
-        (linked_record_id, linked_file_id, linked_thumbnail_file_id, created_at)
-        values (?, ?, ?, now())`,
-      [`${newId}`, `${e.fileId}`, `${e.thumbFileId}`],
-    );
+    values.push([`${newId}`, `${e.fileId}`, `${e.thumbFileId}`, new Date()])
   }
+  await pool.query(
+    `insert into record_item_file
+      (linked_record_id, linked_file_id, linked_thumbnail_file_id, created_at)
+      values ?`,
+    [values],
+  );
 
   res.send({ recordId: newId });
 };
