ee14003ac60d90f6b43e6f25fc93dee438e8d7ef
hayashi-ay <go12limchangyong@gmail.com>
2022-04-17 03:01:42 +0900

    feat: 取得を分けてみた

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -4,6 +4,9 @@ CREATE UNIQUE INDEX linked_record_id_created_at_index ON record_comment(linked_r
 -- select * from record_item_file where linked_record_id = '2cf0aae0-c38d-4cd9-be64-0ffbf04539b3' order by item_id asc limit 1;
 CREATE INDEX linked_record_id_index ON record_item_file(linked_record_id);
 
+-- select record_id from record where status = "closed" order by updated_at desc, record_id asc limit 10 offset 0;
+CREATE UNIQUE INDEX status_updated_at_index ON record(status, updated_at);
+
 CREATE INDEX value_index on session(value);
 CREATE INDEX user_id_index on group_member(user_id);
 CREATE INDEX linked_record_id_index on record_comment(linked_record_id);
