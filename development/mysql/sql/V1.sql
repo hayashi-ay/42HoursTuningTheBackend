2033305255fd6194023b305ffe1e5a071d779e08
hayashi-ay <go12limchangyong@gmail.com>
2022-04-17 00:54:55 +0900

    fix: indexを追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,6 +1,9 @@
 -- select * from record_comment where linked_record_id = '82999' order by created_at desc;
 CREATE UNIQUE INDEX linked_record_id_created_at_index ON record_comment(linked_record_id, created_at);
 
+-- select * from record_item_file where linked_record_id = '2cf0aae0-c38d-4cd9-be64-0ffbf04539b3' order by item_id asc limit 1;
+CREATE INDEX linked_record_id_index ON record_item_file(linked_record_id);
+
 CREATE INDEX value_index on session(value);
 CREATE INDEX user_id_index on group_member(user_id);
 CREATE INDEX linked_record_id_index on record_comment(linked_record_id);
