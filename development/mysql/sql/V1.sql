a6b50f8e429a761697b8eae36ca51d049c59ff73
hayashi-ay <go12limchangyong@gmail.com>
2022-04-16 20:23:50 +0900

    feat: record_commentに対する複合インデックスを追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -2,3 +2,4 @@ CREATE INDEX value_index on session(value);
 CREATE INDEX user_id_index on group_member(user_id);
 CREATE UNIQUE INDEX updated_at_record_id_index ON record (updated_at, record_id);
 CREATE INDEX linked_record_id_index on record_comment(linked_record_id);
+CREATE UNIQUE INDEX linked_record_id_created_at_index ON record_comment (linked_record_id, created_at);
